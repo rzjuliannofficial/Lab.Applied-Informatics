47e7fd907a8430e3a58e2fea80dcf32d1826bbd0
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-11-04 16:36:15 +0700

    add logo

--- components/header.php
+++ components/header.php
@@ -1,8 +1,8 @@
 <header class="sticky-header">
     <nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 flex items-center justify-between">
         <div class="flex items-center gap-2">
-            <div>
-                <img src="/img/AI_Logo.png" alt="">
+            <div style="width: 2.5rem; height: 2.5rem;">
+                <img src="img/logo.png" alt="" style="width: 100%; height: 100%; object-fit: contain;">
             </div>
             <span class="font-bold text-xl text-white">Lab AI</span>
         </div>
