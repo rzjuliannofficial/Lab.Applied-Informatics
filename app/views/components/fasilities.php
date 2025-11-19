7442f5d7776a7a4dedabc9fbc646aa86204826b5
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-11-19 13:49:22 +0700

    add content for fasilities

--- app/views/components/fasilities.php
+++ app/views/components/fasilities.php
@@ -4,7 +4,15 @@
 </div>
 <section id="fasilities" class="section">
     <div class="container">
-        .max-wid
+        <div class="project header">
+            <div class="title">
+            <!-- Ikon Grup Orang (SVG Inline) -->
+                <i class="fas fa-cloud text-xs mr-2"></i> OUR FACILITIES
+            </div>
+            <p class="secondary-title">Explore Our Space <span>Space</span></p>
+            <p class="text-center text-gray-500 mb-12">Comfort and convenience, designed just for you.</p>
+        </div>
+        
     </div>
 </section>
 <div class="shadow-bar-top">
