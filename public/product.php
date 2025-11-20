2638ebb1c0084335b5de1c7f7261cfdea8b4a45a
Kamila Zahwa <kamilazhwa@gmail.com>
2025-11-20 10:42:11 +0700

    adding favicon

--- public/product.php
+++ public/product.php
@@ -68,6 +68,7 @@ if (empty($products)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Our Products - Lab AI Polinema</title>
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="style/Style.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
