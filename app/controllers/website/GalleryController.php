6228a109ee00241aef25a7ce0c58b836dcb48c64
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-11-30 11:34:10 +0700

    update controller untuk mengambil function dari models galeri

--- app/controllers/website/GalleryController.php
+++ app/controllers/website/GalleryController.php
@@ -3,7 +3,31 @@ class GalleryController extends Controller
 {
     public function index()
     {
-        $data['products'] = $this->model('Produk')->getAll();
+        $galeriModel = $this->model('Galeri');
+
+        // Ambil data galeri berdasarkan kategori
+        $data['galeriPublikasi'] = $galeriModel->getByPublikasiLab();
+        $data['galeriBerita'] = $galeriModel->getByBerita();
+        $data['galeriProduk'] = $galeriModel->getByProduk();
+        $data['galeriFasilitas'] = $galeriModel->getByFasilitas();
+        $data['galeriPenelitian'] = $galeriModel->getByPenelitianLab();
+        $data['galeriKegiatan'] = $galeriModel->getByKegiatanLab();
+
+        // Gabungkan semua galeri untuk carousel
+        $data['galleryItems'] = array_merge(
+            $data['galeriPublikasi'],
+            $data['galeriBerita'],
+            $data['galeriProduk'],
+            $data['galeriFasilitas'],
+            $data['galeriPenelitian'],
+            $data['galeriKegiatan']
+        );
+
+        // Urutkan berdasarkan tanggal terbaru
+        usort($data['galleryItems'], function($a, $b) {
+            return strtotime($b['tanggal_upload']) - strtotime($a['tanggal_upload']);
+        });
+
         $data['title'] = 'Lab Applied Informatics Polinema';
 
         $this->view("public/layouts/header", $data);
