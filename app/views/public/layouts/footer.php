25f718dafaa5f9a5d3fa80e937524e0ab3b70ba8
Kamila Zahwa <kamilazhwa@gmail.com>
2025-12-03 11:32:57 +0700

    fix script path in footer layout

--- app/views/public/layouts/footer.php
+++ app/views/public/layouts/footer.php
@@ -99,5 +99,5 @@
 </body>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-<script src="script/script.js"></script>
+<script src="/script/script.js"></script>
 </html>
